a9d73fd4c4b5b01277adbc9133e40c3e6dc137ed
Richard Orlowski <r0rlo@yahoo.com>
2014-06-16 23:27:31 -0700

    update6

--- Menu.cpp
+++ Menu.cpp
@@ -229,11 +229,9 @@ string Menu::List(const DataBase& d, string choice)
 	}
 }
 
-<<<<<<< HEAD
-void displayTree(pointerToDataRecord p, int level)
-=======
+
 void displayTree(pointerToDataRecord & p, int level)
->>>>>>> FETCH_HEAD
+
 {
 	string tempStr = "";
 	string tempName = p.get_pointer()->get_name();
